b28e623f8d0b5195122ae2f972b284a245f805d4
Elijah Matar <m.matar515@gmail.com>
2024-05-06 17:28:51 -0500

    Added react router and attached logic to specific links. Will allow travelling to different navigation pages without restarting server! yay

--- Portfolio/src/main.jsx
+++ Portfolio/src/main.jsx
@@ -1,9 +1,39 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 import App from "./App.jsx";
+import Error from "./pages/Error.jsx";
+import About from "./pages/About.jsx";
+import Projects from "./pages/Projects.jsx";
+import Resume from "./pages/Resume.jsx";
+import Contact from "./pages/Contact.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <About />,
+      },
+      {
+        path: "/Projects",
+        element: <Projects />,
+      },
+      {
+        path: "/Resume",
+        element: <Resume />,
+      },
+      {
+        path: "/Contact",
+        element: <Contact />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <RouterProvider router={router} />
 );
