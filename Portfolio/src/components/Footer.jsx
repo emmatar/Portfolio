beabc456bb181c94629e0b3ea1d0e2955bd63e7b
Elijah Matar <m.matar515@gmail.com>
2024-05-06 18:22:31 -0500

    added footer. Need to link to app.jsx

--- Portfolio/src/components/Footer.jsx
+++ Portfolio/src/components/Footer.jsx
@@ -1,9 +1,30 @@
-function Footer () {
-    return (
-        <>
-
-        </>
-    )
+function Footer() {
+  return (
+    <footer>
+      <section className="section-content" id="contact">
+        <section id="contact-flex">
+          <a className="contact-info mail" href="mailto:m.matar515@gmail.com">
+            m.matar515@gmail.com
+          </a>
+          <a className="contact-info github" href="https://github.com/emmatar">
+            <img id="github-logo" src="./assets/images/github-mark.png" />
+            GitHub
+          </a>
+          <a
+            className="contact-info linkedin"
+            href="https://www.linkedin.com/in/elijah-m-818597b1/"
+          >
+            <img
+              id="linkedin-logo"
+              src="./assets/images/linkedin.svg"
+              alt="Linkedin logo"
+            />
+            Linkedin
+          </a>
+        </section>
+      </section>
+    </footer>
+  );
 }
 
 export default Footer;
