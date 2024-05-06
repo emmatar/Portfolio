6d6723243f1bb83317317a4efdeab78ec46790bd
Elijah Matar <m.matar515@gmail.com>
2024-05-06 18:18:48 -0500

    Included NavTab jsx to link to according page. All within Header component

---
+++ Portfolio/src/components/NavTabs.jsx
@@ -0,0 +1,59 @@
+import { Link, useLocation } from "react-router-dom";
+
+function NavTabs() {
+  const currentPage = useLocation().pathname;
+
+  return (
+    <>
+      <section className="menu">
+        <button className="menu-button">
+          <div className="menu-botton-logo"></div>
+          <div className="menu-botton-logo"></div>
+          <div className="menu-botton-logo"></div>
+        </button>
+        <ul className="menu-content">
+          <li className="nav-item">
+            <Link
+              to="/"
+              className={currentPage === "/" ? "nav-link active" : "nav-link"}
+            >
+              About
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link
+              to="/Projects"
+              className={
+                currentPage === "/Projects" ? "nav-link active" : "nav-link"
+              }
+            >
+              Projects
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link
+              to="/Resume"
+              className={
+                currentPage === "/Resume" ? "nav-link active" : "nav-link"
+              }
+            >
+              Resume
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link
+              to="/Contact"
+              className={
+                currentPage === "/Contact" ? "nav-link active" : "nav-link"
+              }
+            >
+              Contact
+            </Link>
+          </li>
+        </ul>
+      </section>
+    </>
+  );
+}
+
+export default NavTabs;
