5df34e4fe7caf9a6be7a8562d2bcb4c2ccd6b7b7
Elijah Matar <m.matar515@gmail.com>
2024-05-06 18:02:12 -0500

    Created jsx structure for header. Importing NavTabs as their own component for better modularization

---
+++ Portfolio/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import NavTabs from './NavTabs'
+
+function Header() {
+  return (
+    <header>
+      <NavTabs />
+      <div className="name-container">
+        <h1 id="name">Elijah Matar</h1>
+        <img src="../../public/Profile_pic.jpeg" alt="Profile picture" />
+      </div>
+    </header>
+  );
+}
+
+export default Header;
