d8363d38e2438ae2d11fd8d7c9f6649d4b9fe7f6
Elijah Matar <m.matar515@gmail.com>
2024-05-07 15:06:34 -0500

    refactored old code, and implemented react-bootstrap while deleting excessive css file

--- Portfolio/src/components/Footer.jsx
+++ Portfolio/src/components/Footer.jsx
@@ -1,28 +1,35 @@
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
 function Footer() {
   return (
-    <footer>
-      <section className="section-content" id="contact">
-        <section id="contact-flex">
-          <a className="contact-info mail" href="mailto:m.matar515@gmail.com">
-            m.matar515@gmail.com
-          </a>
-          <a className="contact-info github" href="https://github.com/emmatar">
-            <img id="github-logo" src="../../public/github-mark.png" />
-            GitHub
-          </a>
-          <a
-            className="contact-info linkedin"
-            href="https://www.linkedin.com/in/elijah-m-818597b1/"
-          >
-            <img
-              id="linkedin-logo"
-              src="../../public/linkedin.svg"
-              alt="Linkedin logo"
-            />
-            Linkedin
-          </a>
-        </section>
-      </section>
+    <footer className="fixed-bottom">
+      <Container fluid>
+        <Row className="d-flex justify-content-center">
+          <Col xs={2} className="d-flex justify-content-center align-items-center p-0">
+            <a className="contact-info mail" href="mailto:m.matar515@gmail.com">
+              Email
+            </a>
+          </Col>
+          <Col xs={2} className="d-flex justify-content-center align-items-center p-0">
+            <a
+              className="contact-info github"
+              href="https://github.com/emmatar"
+            >
+              GitHub
+            </a>
+          </Col>
+          <Col xs={2} className="d-flex justify-content-center align-items-center p-0">
+            <a
+              className="contact-info linkedin"
+              href="https://www.linkedin.com/in/elijah-m-818597b1/"
+            >
+              Linkedin
+            </a>
+          </Col>
+        </Row>
+      </Container>
     </footer>
   );
 }
