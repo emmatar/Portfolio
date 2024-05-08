d5860300a55f20ca82b29d33094d8eb9e54e425b
Elijah Matar <m.matar515@gmail.com>
2024-05-08 17:09:49 -0500

    Base structuring for layout

--- Portfolio/src/pages/Contact.jsx
+++ Portfolio/src/pages/Contact.jsx
@@ -1,5 +1,18 @@
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
 function Contact() {
-  return;
+  return (
+    <Container lg={12} className="p-2">
+      <h1 className="page-title-style">Contact</h1>
+      <Col className="page-section-styles">
+        <Row className="mx-4">
+          
+        </Row>
+      </Col>
+    </Container>
+  );
 }
 
 export default Contact;
--- Portfolio/src/pages/Resume.jsx
+++ Portfolio/src/pages/Resume.jsx
@@ -1,5 +1,18 @@
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
 function Resume() {
-  return;
+  return (
+    <Container lg={12} className="p-2">
+      <h1 className="page-title-style">Resume</h1>
+      <Col className="page-section-styles">
+        <Row className="mx-4">
+          
+        </Row>
+      </Col>
+    </Container>
+  );
 }
 
 export default Resume;
